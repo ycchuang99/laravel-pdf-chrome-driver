691854825c127fe853818f4730361bb5c5df040c
Evan Chuang <evanchuang@starlux-airlines.com>
2026-03-12 09:44:52 +0800

    style: format code for consistency in ChromePhpDriverServiceProvider

--- src/ChromePhpDriverServiceProvider.php
+++ src/ChromePhpDriverServiceProvider.php
@@ -12,28 +12,10 @@ class ChromePhpDriverServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/chrome-php.php', 'laravel-pdf.chrome-php');
+        $this->mergeConfigFrom(__DIR__ . '/../config/chrome-php.php', 'laravel-pdf.chrome-php');
 
         $this->app->singleton('laravel-pdf.driver.chrome-php', function () {
-            return new ChromePhpDriver(
-                config('laravel-pdf.chrome-php', []),
-            );
-        });
-
-        $this->app->singleton(PdfDriver::class, function ($app) {
-            $driverName = config('laravel-pdf.driver', 'browsershot');
-
-            if ($driverName === 'chrome-php') {
-                return $app->make('laravel-pdf.driver.chrome-php');
-            }
-
-            $key = "laravel-pdf.driver.{$driverName}";
-
-            if ($app->bound($key)) {
-                return $app->make($key);
-            }
-
-            throw InvalidDriver::unknown($driverName);
+            return new ChromePhpDriver(config('laravel-pdf.chrome-php', []));
         });
     }
 
@@ -41,7 +23,7 @@ class ChromePhpDriverServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/chrome-php.php' => config_path('laravel-pdf-chrome-php.php'),
+                __DIR__ . '/../config/chrome-php.php' => config_path('laravel-pdf-chrome-php.php'),
             ], 'chrome-php-config');
         }
     }
